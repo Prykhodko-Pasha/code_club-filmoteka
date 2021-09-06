e76d1642090f318b30ecee97df47342d111847d4
SovaV <sova.volodymyr95@gmail.com>
2021-09-06 13:59:52 +0300

    commit

--- src/js/homeHeader.js
+++ src/js/homeHeader.js
@@ -1,29 +1,3 @@
-// import Header from '../templates/hpmeHeader.hbs';
-// import Library from '../templates/library.hbs';
-// const renderHeader = document.querySelector('#headerHome');
-// const renderLibrary = document.querySelector('#headLibr');
-
-// renderHeader.insertAdjacentHTML('beforeend', Header());
-
-// const btnLibrary = document.querySelector('#library');
-// btnLibrary.addEventListener('click', onLibrary);
-
-// function onLibrary() {
-//   renderLibrary.insertAdjacentHTML('beforeend', Library());
-//   renderHeader.innerHTML = '';
-//   console.log('Либрари');
-
-//   const btnHome = document.querySelector('#home');
-//   console.log(btnHome, 'добвил кнопку ХОМЕ');
-//   btnHome.addEventListener('click', onHome);
-// }
-
-// function onHome() {
-//   renderHeader.insertAdjacentHTML('beforeend', Header());
-//   renderLibrary.innerHTML = '';
-//   console.log('Хоме');
-// }
-
 import Library from '../templates/library.hbs';
 const renderLibrary = document.querySelector('#headLibr');
 const includeHeader = document.querySelector('.header__main');
