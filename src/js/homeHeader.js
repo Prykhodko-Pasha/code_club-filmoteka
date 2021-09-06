0e14fa8123c1bed92ff46a43d172a339d10929b6
SovaV <sova.volodymyr95@gmail.com>
2021-09-06 14:22:11 +0300

    cmmit

--- src/js/homeHeader.js
+++ src/js/homeHeader.js
@@ -1,18 +1,20 @@
 import Library from '../templates/library.hbs';
 const renderLibrary = document.querySelector('#headLibr');
 const includeHeader = document.querySelector('.header__main');
+const includeMain = document.querySelector('.film-gallery-section');
 
 const btnLibrary = document.querySelector('#library');
 btnLibrary.addEventListener('click', onLibrary);
 
 function onLibrary() {
   renderLibrary.insertAdjacentHTML('beforeend', Library());
-  includeHeader.style.cssText = `display: none`;
   const btnHome = document.querySelector('#home');
   btnHome.addEventListener('click', onHome);
-}
-
-function onHome() {
-  renderLibrary.innerHTML = '';
-  includeHeader.style.cssText = `display: block`;
+  includeHeader.style.cssText = `display: none`;
+  includeMain.style.cssText = `display: none`;
+  function onHome() {
+    renderLibrary.innerHTML = '';
+    includeHeader.style.cssText = `display: block`;
+    includeMain.style.cssText = `display: block`;
+  }
 }
