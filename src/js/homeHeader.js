098f4cb1c40b71f4019c5e240c1a2d96c9d04cf1
SovaV <sova.volodymyr95@gmail.com>
2021-09-06 13:05:43 +0300

    отрисовка хедер

--- src/js/homeHeader.js
+++ src/js/homeHeader.js
@@ -1,30 +1,44 @@
-import Header from '../templates/hpmeHeader.hbs';
+// import Header from '../templates/hpmeHeader.hbs';
+// import Library from '../templates/library.hbs';
+// const renderHeader = document.querySelector('#headerHome');
+// const renderLibrary = document.querySelector('#headLibr');
+
+// renderHeader.insertAdjacentHTML('beforeend', Header());
+
+// const btnLibrary = document.querySelector('#library');
+// btnLibrary.addEventListener('click', onLibrary);
+
+// function onLibrary() {
+//   renderLibrary.insertAdjacentHTML('beforeend', Library());
+//   renderHeader.innerHTML = '';
+//   console.log('Либрари');
+
+//   const btnHome = document.querySelector('#home');
+//   console.log(btnHome, 'добвил кнопку ХОМЕ');
+//   btnHome.addEventListener('click', onHome);
+// }
+
+// function onHome() {
+//   renderHeader.insertAdjacentHTML('beforeend', Header());
+//   renderLibrary.innerHTML = '';
+//   console.log('Хоме');
+// }
+
 import Library from '../templates/library.hbs';
-
-const renderHeader = document.querySelector('#headerHome');
 const renderLibrary = document.querySelector('#headLibr');
-
-renderHeader.insertAdjacentHTML('beforeend', Header());
-renderLibrary.insertAdjacentHTML('beforeend', Library());
+const includeHeader = document.querySelector('.header__main');
 
 const btnLibrary = document.querySelector('#library');
-console.log({ btnLibrary });
-
 btnLibrary.addEventListener('click', onLibrary);
-btnHome.addEventListener('click', onHome);
-
-const btnHome = document.querySelector('#home');
-console.log('Хоме');
 
 function onLibrary() {
   renderLibrary.insertAdjacentHTML('beforeend', Library());
-  renderHeader.innerHTML = '';
-  console.log('Либрари');
-}
-function onHome() {
-  console.log('Хоме');
-  renderHeader.insertAdjacentHTML('beforeend', Header());
-  renderLibrary.innerHTML = '';
+  includeHeader.style.cssText = `display: none`;
+  const btnHome = document.querySelector('#home');
+  btnHome.addEventListener('click', onHome);
 }
 
-// ===============================
+function onHome() {
+  renderLibrary.innerHTML = '';
+  includeHeader.style.cssText = `display: block`;
+}
