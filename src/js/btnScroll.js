932f0b94192d8ecb0c6c6b4aec69f4d4412a5cef
SovaV <sova.volodymyr95@gmail.com>
2021-09-07 23:24:38 +0300

    scroll

--- src/js/btnScroll.js
+++ src/js/btnScroll.js
@@ -1,6 +1,6 @@
 import debounce from 'lodash.debounce';
 
-const btnUp = document.querySelector('.flowing-scroll');
+const btnUp = document.querySelector('.flowing__scroll-up');
 
 window.addEventListener('scroll', debounce(hiddenScroll(btnUp)));
 btnUp.addEventListener('click', scrollUp);
@@ -19,3 +19,25 @@ function hiddenScroll(e) {
 function scrollUp() {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+
+// ============================================================
+
+// const btnDown = document.querySelector('.flowing__scroll-down');
+
+// window.addEventListener('scroll', debounce(hiddenScroll(btnDown)));
+// btnDown.addEventListener('click', scrollUp);
+
+
+// function hiddenScroll(e) {
+//   return function hideOnScroll() {
+//     if (scrollY < document.documentElement.clientHeight) {
+//       e.classList.remove('hidden__scroll');
+//       e.classList.add('hidden__scroll');
+//     } else {
+//     }
+//   };
+// }
+
+// function scrollUp() {
+//   window.scrollTo({ top: 0, behavior: 'smooth' });
+// }
