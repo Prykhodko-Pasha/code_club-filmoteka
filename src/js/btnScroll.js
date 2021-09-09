3ab70e071a76b5b12e6b6ce6e54e785d98d1e180
SovaV <sova.volodymyr95@gmail.com>
2021-09-09 17:50:13 +0300

    finish scroll

--- src/js/btnScroll.js
+++ src/js/btnScroll.js
@@ -1,6 +1,8 @@
 const btnUp = document.querySelector('.flowing__scroll-up');
 const btnDown = document.querySelector('.flowing__scroll-down');
 const vwBody = document.querySelector('body');
+const footer = document.querySelector('.footer');
+
 
 window.addEventListener('scroll', hiddenScroll(btnUp));
 btnUp.addEventListener('click', scrollUp);
@@ -30,3 +32,18 @@ function scrollDown() {
     behavior: 'smooth',
   });
 }
+
+function onEntry(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      btnDown.classList.add('hidden__scroll');
+    } else {
+      btnDown.classList.remove('hidden__scroll');
+    }
+  });
+}
+
+const options = {};
+
+const observer = new IntersectionObserver(onEntry, options);
+observer.observe(footer);
